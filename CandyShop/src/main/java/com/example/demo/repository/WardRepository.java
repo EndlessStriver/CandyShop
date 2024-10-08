0dd18e6e8475fdc644fe5b1830b6341e9b9aad06
thienphu <phutot1111@gmail.com>
2024-10-08 09:47:15 +0700

    khoi tao cac repository cho model

---
+++ CandyShop/src/main/java/com/example/demo/repository/WardRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Ward;
+
+@Repository
+public interface WardRepository extends JpaRepository<Ward, String>{
+
+}
