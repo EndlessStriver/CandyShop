7bcf8318bb4201efaa415ebfdd715991ee591d57
thienphu <phutot1111@gmail.com>
2024-11-04 18:10:31 +0700

    bo sung api xoa anh theo id image

--- CandyShop/src/main/java/com/example/demo/controller/ImageController.java
+++ CandyShop/src/main/java/com/example/demo/controller/ImageController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.ApiResponseDTO;
 import com.example.demo.service.ImageService;
 
 @RestController
@@ -17,8 +21,10 @@ public class ImageController {
 	}
 	
 	@DeleteMapping("/{id}")
-	public void deleteImage(String imageId) throws Exception {
-		imageService.deleteImage(imageId);
+	public ResponseEntity<?> deleteImage(@PathVariable String id) throws Exception {
+		imageService.deleteImage(id);
+		ApiResponseDTO<Void> response = new ApiResponseDTO<>("Image deleted successfully", HttpStatus.OK.value(), null);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 	
 }
